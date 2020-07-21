8ea3b657a2898485c9c65f81dea5174ec0bbf3ab
tam nguyen <ndtam.273@gmail.com>
2020-07-21 22:08:24 +0700

    rwrwrewrwrwerwer

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('The meal!'),);
+  }
+}
