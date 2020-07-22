bd66b03cfc55d7475468097ea19984869c25ae8c
tam nguyen <ndtam.273@gmail.com>
2020-07-22 21:49:02 +0700

    werwerr

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TabScreen extends StatefulWidget {
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.category),
+              text: 'Categories',
+            ),
+            Tab(
+              icon: Icon(Icons.favorite),
+              text: 'Faforites',
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
